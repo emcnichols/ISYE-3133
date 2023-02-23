7e2656eb8f2f41f58a3126ae9d1e286b50652b32
chettrich15 <103069852+chettrich15@users.noreply.github.com>
2023-02-23 13:08:54 -0500

    Add files via upload

---
+++ ISYE-3133-PA02.py
@@ -0,0 +1,9 @@
+# Import statements
+import pandas as pd
+from gurobipy import *
+
+# Open and assign file variable names
+demand_data_df = pd.read_csv("demand_data.csv")
+cost_data_df = pd.read_csv("cost_data.csv")
+product_data_df = pd.read_csv("product_data.csv")
+volume_data_df = pd.read_csv("volume_data.csv")
